72447f73ec191b0a3e76f400404d89499ff0b8eb
Chester Lynn <chester.htooaunglinn@gmail.com>
2020-12-07 00:11:47 +0630

    change some colors

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.views import generic
 from .forms import CommentForm
+from django.contrib.auth.decorators import login_required
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
